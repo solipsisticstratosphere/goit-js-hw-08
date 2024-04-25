dc674d828e47a8cc3c068d7c8cc3c9d10482c364
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-04-25 16:29:17 +0300

    dz2

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,13 @@
-function calculateTotal(number) {
-  let sum = 0;
-  let smth = 1;
-  while (smth <= number) {
-    sum += smth;
-    smth += 1;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const sum = quantity * pricePerDroid;
+  if (customerCredits < sum) {
+    return `Insufficient funds!`;
+  } else {
+    return `You ordered ${quantity} droids worth ${sum} credits!`;
   }
-  return sum;
 }
-
-console.log(calculateTotal(18)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
