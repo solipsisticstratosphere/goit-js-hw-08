ec4f89b69f494e359d5b21cbd7e0d2acc9ab98ab
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-05-19 19:29:21 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,15 @@
-function slugify(title) {
-  const LowerCase = title.toLowerCase();
-  const words = LowerCase.split(' ');
-  const slug = words.join('-');
-  return slug;
+function getAllPropValues(propName) {
+  const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+  const smth = [];
+  for (let product of products) {
+    smth.push(product[propName]);
+  }
+  return smth;
 }
 
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(getAllPropValues('name')); // "arrays-for-begginers"
